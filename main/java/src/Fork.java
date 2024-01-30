5f378c54da26d0af345b550eabdbc12317ec1b3c
Chizuru <chizuru.yamauchi@dci-student.org>
2024-01-30 18:17:24 +0100

    modify Philosopher class

--- main/java/src/Fork.java
+++ main/java/src/Fork.java
@@ -5,8 +5,9 @@ import java.util.concurrent.locks.ReentrantLock;
 // represents a fork that philosopher can pick up and put down
 public class Fork { // remove the Runnable interface since it is not a task that runs in a separate thread
 
-    // control the access to the fork = ensuring that only one philosopher can pick up the fork at a time
-    private Lock lock = new ReentrantLock(); // Lock is more generic = more flexible for testability and more
+    // control the access to the fork with lock = ensuring that only one philosopher can pick up the fork at a time
+    // making it final = ensures that the reference to the ReentrantLock instance cannot be changed, preventing potential issues related to concurrency amd thread safety
+    private final Lock lock = new ReentrantLock(); // Lock is more generic = more flexible for testability and more
 
     // take the fork
     public void pickUp(){
--- main/java/src/Philosophers.java
+++ main/java/src/Philosophers.java
@@ -1,11 +1,12 @@
 
-
+ // represents a philosopher that thinks, eats, and pick up and put down a fork
 public class Philosophers implements Runnable{
     // attributes
-    private int philosopherID;
-    private Fork leftFork;
-    private Fork rightFork;
-    private int priority;
+    // making it final = not meant to be changed or overridden
+    private final int philosopherID;
+    private final Fork leftFork;
+    private final Fork rightFork;
+    private final int priority;
 
     // constructor
     public Philosophers(int philosopherID, Fork leftFork, Fork rightFork, int priority){
@@ -20,31 +21,55 @@ public class Philosophers implements Runnable{
 
         while(true) {
             try{
+                // while true, philosopher think, use fork, eat finish using fork
             think();
             pickUpForks();
             eat();
             putDownForks();
-            }catch(InterruptedException e){
-
+            }catch(InterruptedException e){ // handle interruption (ex. thread termination)
+                Thread.currentThread().interrupt();
             }
         }
     }
 
     private void think() throws InterruptedException{
-
-
+        System.out.println("Philosopher " + philosopherID + " is thinking.");
+        Thread.sleep(1000); // simulate thinking time: 1 second duration
     }
 
     private void pickUpForks() throws InterruptedException{
+        System.out.println("Philosopher " + philosopherID + " is waiting for forks.");
+
+        // pick up left fork
+        leftFork.pickUp();
+        System.out.println("Philosopher " + philosopherID + " picked up left fork.");
+
+        // pick up right fork
+        rightFork.pickUp();
+        System.out.println("Philosopher " + philosopherID + " picked up right fork.");
+
 
     }
 
     private void eat()throws InterruptedException{
+        System.out.println("Philosopher " + philosopherID + " is eating.");
+        Thread.sleep(2000); // simulate eating time: 2 sec
 
     }
 
     private void putDownForks() throws InterruptedException{
+        // put down left fork
+        leftFork.putDown();
+        System.out.println("Philosopher " + philosopherID + "put down left fork.");
 
+        // put down right fork
+        rightFork.putDown();
+        System.out.println("Philosopher " + philosopherID + "put down right fork.");
+    }
+
+    // getter for the priority of the philosopher
+    public int getPriority(){
+        return priority;
     }
 
 
